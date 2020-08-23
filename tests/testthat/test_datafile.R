efebbbd09d20324a6a33af463bf6af4ecd8b87ef
mabelolaya <mabel.olaya.perez@gmail.com>
2020-08-23 01:34:12 -0500

    finalmente

---
+++ tests/testthat/test_datafile.R
@@ -0,0 +1,7 @@
+
+test_that(".csv data files are available", {
+  testthat::expect_equal(list.files(system.file("extdata", package = "mypackage")),
+                         c("accident_2013.csv.bz2",
+                           "accident_2014.csv.bz2",
+                           "accident_2015.csv.bz2"))
+})
